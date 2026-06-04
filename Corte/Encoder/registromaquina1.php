435c0828615654fb842c8fe41a958554a026fb01
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-04 16:08:30 -0500

    ese

--- Corte/Encoder/registromaquina1.php
+++ Corte/Encoder/registromaquina1.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'app.php';
 $date = date('Y-m-d');
-$qry = "SELECT * FROM lecturas WHERE STR_TO_DATE(fecha, '%Y-%m-%d') = STR_TO_DATE('$date', '%Y-%m-%d') ORDER BY maquina, id DESC limit 50";
+$maquina = $_GET['maquina'];
+$qry = "SELECT * FROM lecturas WHERE STR_TO_DATE(fecha, '%Y-%m-%d') = STR_TO_DATE('$date', '%Y-%m-%d') AND maquina = '$maquina' ORDER BY maquina, id DESC limit 50";
 $data = $conn->query($qry);
 
 echo json_encode($data->fetch_all(MYSQLI_ASSOC));
